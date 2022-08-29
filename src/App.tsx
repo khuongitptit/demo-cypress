b94114b12c3c2b03e4d4af173dd36b4ca7066765
Nguyen Danh Khuong <nguyendanhkhuong@toilakhuong.local>
2022-08-29 11:00:39 +0700

    products page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import "styles/global.css";
-
-import Home from "pages";
+import 'styles/global.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from 'components/home';
+import Products from 'components/products';
 
 function App(): JSX.Element {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+  const renderRoutes = () => {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/products">Products</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/products" exact>
+              <Products />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  };
+  return <div className="App">{renderRoutes()}</div>;
 }
 
 export default App;
